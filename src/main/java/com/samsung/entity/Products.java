704d8ce62620c329e9dbb7c13c002c4ec3438775
Jasjot <jasjot_jrarjn1@yahoo.in>
2020-05-22 19:39:44 -0700

    enable eureka client and change pom file to generate a jar.

--- src/main/java/com/samsung/entity/Products.java
+++ src/main/java/com/samsung/entity/Products.java
@@ -1,2 +1,40 @@
 package com.samsung.entity;
 
+public class Products {
+    int id;
+    String name;
+    String type;
+
+    public Products() {
+    }
+
+    public Products(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
